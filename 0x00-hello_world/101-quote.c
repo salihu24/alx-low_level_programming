c2f33021f22de5c8f2e7ff3ae3ac25e66337505b
Salihu Abdul <salihuabdul3@gmail.com>
2022-09-08 22:07:06 +0000

    8th commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-
+#include <unistd.h>
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- */
+ *  * main - prints exactly "and that piece of art is useful"
+ *   * Return: Always 0 (Success)
+ *    */
 int main(void)
 {
-		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
-
-			return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
