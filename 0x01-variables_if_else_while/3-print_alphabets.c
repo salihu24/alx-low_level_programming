fbce2a3a101ede0ad84b566b748fceecca109148
Salihu Abdul <salihuabdul3@gmail.com>
2022-09-09 15:47:49 +0000

    4th commit

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
+ * main - Entry point
+ *
+ * Return: Always 0 (Success/correct)
  */
 int main(void)
 {
-		char low;
+	char c;
+	char d;
 
-		for (low = 'a'; low <= 'z'; low++)
-						putchar(low);
-				for (low = 'A'; low <= 'Z'; low++)
-							putchar(low);
-						putchar('\n');
-
-							return (0);
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	for (d = 'A'; d <= 'Z'; d++)
+		putchar(d);
+	putchar('\n');
+	return (0);
 }
