07908276293fd597692fbd0a0dd32a0a14805510
Salihu Abdul <salihuabdul3@gmail.com>
2022-09-09 15:51:48 +0000

    third commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
+ * main - Entry point
+ *
+ * Return: Always 0 (Success/correct)
  */
 int main(void)
 {
-		char low;
+	char salihu;
 
-		for (low = 'a'; low <= 'z'; low++)
-			putchar(low);
-			printf("\n");
-
-		return (0);
+	for (salihu = 'a'; salihu <= 'z'; salihu++)
+	{
+		putchar(salihu);
+	}
+	putchar('\n');
+	return (0);
 }
