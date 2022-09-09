03c632df5d9bd84286503768fd8c7293fcb5bed3
Salihu Abdul <salihuabdul3@gmail.com>
2022-09-09 19:07:51 +0000

    The more brains you use, the less material you need

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,27 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
- * main - program compilation begins from main
- * Description: Program to print all possible combinations of numbers
- with no two numbers same in pairing
- * author: salihu
- * Return: 0 means success
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-        int a, b;
+	int d;
 
-        for (a = 48; a <=57; a++)
-        {
-                for (b = 49; b <= 57; b++)
-                {
-                        if ( a != b && b > a)
-                        {
-                                putchar(a);
-                                putchar(b);
-                                putchar(',');
-                        }
-                }
-        }
-        putchar('\n');
-        return (0);
+	for (d = 0; d < 90; d++)
+	{
+		for ()
+		{
+			putchar((d / 10) + '0');
+			putchar((d % 10) + '0');
+		}
+		if (d != 89)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
 }
