cbbdbe5a29af7f758c424051c5c03584e1f95358
Salihu Abdul <salihuabdul3@gmail.com>
2022-09-09 15:11:36 +0000

    ninth commit

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,21 +1,19 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+
 /**
  * main - Entry point
- *
+ * autor: salihu
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int d;
-	char low;
+	int salihu;
+	char abdul;
 
-	for (d = '0'; d <= '9'; d++)
-		putchar(d);
-	for (low = 'a'; low <= 'f'; low++)
-		putchar(low);
-		putchar('\n');
-
+	for (salihu = 48; salihu < 58; salihu++)
+		putchar(salihu);
+	for (abdul = 'a'; abdul <= 'f'; abdul++)
+		putchar(abdul);
+	putchar('\n');
 	return (0);
 }
