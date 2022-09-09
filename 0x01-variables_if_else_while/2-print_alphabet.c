67d040351a4f5081560fd98389b0a479fe65925d
Salihu Abdul <salihuabdul3@gmail.com>
2022-09-09 14:08:17 +0000

    third 1

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,14 +5,14 @@
  *  * main - Entry point
  *   *
  *    * Return: Always 0 (Success)
- *     */
+ */
 int main(void)
 {
 		char low;
 
-			for (low = 'a'; low <= 'z'; low++)
-						putchar(low);
-					printf("\n");
+		for (low = 'a'; low <= 'z'; low++)
+			putchar(low);
+			printf("\n");
 
-						return (0);
+			return (0);
 }
