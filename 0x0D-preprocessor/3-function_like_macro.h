57ebbc4bb550c52b0468cd869be2f7d8d8af5922
Salihu Abdul <salihuabdul3@gmail.com>
2022-10-10 11:16:09 +0000

    third commit for macro

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,6 @@
-#ifndef ABS_X
-#define ABS_X
-#define ABS(X) ((X < 0) ? (X * -1) : (X))
-#endif
+#ifndef FUNCTION_LIKE_MACRO
+#define FUNCTION_LIKE_MACRO
+
+#define ABS(x) (x * ((x < 0) * (-1) + (x > 0)))
+
+#endif /* FUNCTION_LIKE_MACRO */
